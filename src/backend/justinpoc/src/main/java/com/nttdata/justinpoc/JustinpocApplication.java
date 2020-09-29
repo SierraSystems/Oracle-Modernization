50fc07bc02c1099856f217aeff6b47a086cfe094
TayGov <Taylor.Clausen@gov.bc.ca>
2020-09-29 12:01:17 -0700

    some changes

---
+++ src/backend/justinpoc/src/main/java/com/nttdata/justinpoc/JustinpocApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.justinpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustinpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustinpocApplication.class, args);
+	}
+
+}
