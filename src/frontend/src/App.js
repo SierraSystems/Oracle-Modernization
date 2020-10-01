c4ac3fd43df8dd036bec70750ada6cb5d2227e72
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-10-01 14:45:23 -0700

    add table

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Header } from 'shared-components';
+import Agencies from './components/Agencies';
+
+const getAgencies = (setAgencies) => {
+  const agency1 = {
+    id: 1,
+    name: "My Agency",
+    address: "123 Best Street",
+    phoneNumber: "123-456-7890",
+    email: "myemail@example.com",
+    contactPerson: "Dan Wolfe"
+  };
+  const agency2 = { ...agency1, id: 2, contactPerson: "Phillip Price" };
+  const agencies = [agency1, agency2];
+
+  setAgencies(agencies);
+};
 
 function App() {
+  const [agencies, setAgencies] = useState([]);
+
+  useEffect(() => {
+    getAgencies(setAgencies);
+  }, []);
+
   const header = {
     name: "Justin POC",
     history: useHistory(),
@@ -14,6 +36,8 @@ function App() {
       <div className="page">
         <div className="content col-md-12">
           <h1>Justin POC</h1>
+          <br />
+          <Agencies agencies={agencies} />
         </div>
       </div>
     </main>
