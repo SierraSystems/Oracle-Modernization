4954980398fe1060f124d0b93fe0c2580c4687ee
TayGov <Taylor.Clausen@gov.bc.ca>
2020-10-06 10:48:43 -0700

    Other changes

--- src/backend/libs/justin-data-starter/src/main/java/com/nttdata/pocdata/ContactsDao.java
+++ src/backend/libs/justin-data-starter/src/main/java/com/nttdata/pocdata/ContactsDao.java
@@ -1,9 +1,9 @@
 package com.nttdata.pocdata;
 
 import com.nttdata.data.Contacts;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.math.BigDecimal;
 
-public interface ContactsDao extends CrudRepository<Contacts, BigDecimal> { }
+public interface ContactsDao extends JpaRepository<Contacts, BigDecimal> { }
--- src/backend/management-api/src/main/java/com/nttdata/managementapi/ManagementApiApplication.java
+++ src/backend/management-api/src/main/java/com/nttdata/managementapi/ManagementApiApplication.java
@@ -2,12 +2,14 @@ package com.nttdata.managementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = {"com.nttdata.managementapi","com.nttdata.pocdata"})
 @EnableTransactionManagement
 @EnableJpaRepositories("com.nttdata.pocdata")
+@EntityScan("com.nttdata.data")
 public class ManagementApiApplication {
 
 	public static void main(String[] args) {
