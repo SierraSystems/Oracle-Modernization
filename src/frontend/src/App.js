d474b3f0147b351083bcc37361cff8f9281bd9eb
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-10-06 10:27:15 -0700

    text

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -19,10 +19,15 @@ function App() {
       <div className="page">
         <div className="content col-md-12">
           <h1>Oracle Modernization</h1>
-          <p>
-            Welcome to Oracle Modernization. This proof-of-concept application will allow users
-            to interact and perform CRUD operations on entities residing within an Oracle Database.
-          </p>
+          <span>
+            Welcome to Oracle Modernization. A proof-of-concept to migrate legacy Oracle Forms to a modern software architecture.
+          </span>
+          <br />
+          <span>
+            This application will allow users to interact and perform CRUD operations on entities residing within an Oracle Database.
+          </span>
+          <br />
+          <br />
           <br />
           <Contacts />
           <br />
