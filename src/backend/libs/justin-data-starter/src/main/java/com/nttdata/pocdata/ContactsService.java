bc4fdc2b227f4844549b648896eac651b25e938b
TayGov <Taylor.Clausen@gov.bc.ca>
2020-10-06 08:00:35 -0700

    Update api

---
+++ src/backend/libs/justin-data-starter/src/main/java/com/nttdata/pocdata/ContactsService.java
@@ -0,0 +1,13 @@
+package com.nttdata.pocdata;
+
+import com.nttdata.data.Contacts;
+
+public class ContactsService {
+
+    public String getContacts() {
+        Contacts contacts = new Contacts();
+
+        return "WAT";
+    }
+
+}
