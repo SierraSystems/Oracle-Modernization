cef5a4dd170e82e1ca5764d77b870844f2b17c77
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-10-07 14:03:36 -0700

    add loading state

--- src/frontend/src/components/contacts/Contacts.js
+++ src/frontend/src/components/contacts/Contacts.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { MdModeEdit, MdDeleteForever } from 'react-icons/md';
-import { Button } from 'shared-components';
+import { Button, Loader } from 'shared-components';
 import EditContact from '../edit-contact/EditContact';
 import Feedback from '../feedback/Feedback';
 import AddContact from '../add-contact/AddContact';
@@ -56,44 +56,49 @@ export default function Contacts() {
         <Button label="Add New Contact" styling="normal-blue btn" onClick={() => setAddContact(true)} />
       </div>
       <br />
-      <table className="table table-striped table-hover table-bordered table-sm">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Customer ID</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map(contact => (
-            <tr key={contact.contactId}>
-              <td>{contact.contactId}</td>
-              <td>{contact.firstName}</td>
-              <td>{contact.lastName}</td>
-              <td>{contact.email}</td>
-              <td>{contact.phoneNumber}</td>
-              <td>{contact.customerId}</td>
-              <td>
-                <div className="icon-spacing">
-                  <MdModeEdit className="pointer" size={32} onClick={() => setContactToEdit(contact)} />
-                  <MdDeleteForever
-                    className="pointer"
-                    size={32}
-                    onClick={() => {
-                      setContactToDelete(contact);
-                      setShowModal(true);
-                    }}
-                  />
-                </div>
-              </td>
+      {contacts.length === 0 && (
+        <Loader page />
+      )}
+      {contacts.length > 0 && (
+        <table className="table table-striped table-hover table-bordered table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Customer ID</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {contacts.map(contact => (
+              <tr key={contact.contactId}>
+                <td>{contact.contactId}</td>
+                <td>{contact.firstName}</td>
+                <td>{contact.lastName}</td>
+                <td>{contact.email}</td>
+                <td>{contact.phoneNumber}</td>
+                <td>{contact.customerId}</td>
+                <td>
+                  <div className="icon-spacing">
+                    <MdModeEdit className="pointer" size={32} onClick={() => setContactToEdit(contact)} />
+                    <MdDeleteForever
+                      className="pointer"
+                      size={32}
+                      onClick={() => {
+                        setContactToDelete(contact);
+                        setShowModal(true);
+                      }}
+                    />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
       <br />
       <br />
       <Feedback />
