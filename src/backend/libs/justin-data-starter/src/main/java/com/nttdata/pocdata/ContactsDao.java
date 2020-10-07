935458f53d3926cddf41a92b6f108f75e1d6210d
TayGov <Taylor.Clausen@gov.bc.ca>
2020-10-07 12:09:08 -0700

    Its working?

--- src/backend/libs/justin-data-starter/src/main/java/com/nttdata/pocdata/ContactsDao.java
+++ src/backend/libs/justin-data-starter/src/main/java/com/nttdata/pocdata/ContactsDao.java
@@ -4,6 +4,8 @@ import com.nttdata.pocdata.hibernate.Contacts;
 
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.math.BigInteger;
 
+@Transactional
 public interface ContactsDao extends CrudRepository<Contacts, BigInteger> { }
--- src/backend/libs/justin-data-starter/src/main/java/com/nttdata/pocdata/ContactsServiceImpl.java
+++ src/backend/libs/justin-data-starter/src/main/java/com/nttdata/pocdata/ContactsServiceImpl.java
@@ -3,7 +3,6 @@ package com.nttdata.pocdata;
 import com.nttdata.pocdata.hibernate.Contacts;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
