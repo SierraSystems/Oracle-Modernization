1e50d0e7913a14e19a9a82c5eef99ba79db7b506
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-10-07 14:42:39 -0700

    add and edit functionality

--- src/frontend/src/components/edit-contact/EditContact.js
+++ src/frontend/src/components/edit-contact/EditContact.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { Button, Input } from 'shared-components';
 import Contacts from '../contacts/Contacts';
 import './EditContact.css';
@@ -31,6 +32,17 @@ const generateInput = (id, label, value, setEditedContact, editedContact) => {
   );
 };
 
+const editContact = (editedContact, setShowContacts) => {
+  axios
+    .put('/management/contact/update', editedContact)
+    .then((res) => {
+      if (res.status === 200) setShowContacts(true);
+    })
+    .catch((err) => {
+      console.log('Error editing contact: ', err);
+    })
+};
+
 export default function EditContact({ contact }) {
   const [editedContact, setEditedContact] = useState(contact);
   const [showContacts, setShowContacts] = useState(false);
@@ -52,6 +64,7 @@ export default function EditContact({ contact }) {
         <Button
           label="Edit Contact"
           styling="normal-blue btn space-right"
+          onClick={() => editContact(editedContact, setShowContacts)}
         />
         <Button
           label="Cancel"
