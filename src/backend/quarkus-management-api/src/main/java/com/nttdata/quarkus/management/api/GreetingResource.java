2e7e7679f2409a384fa3547a245da266f752a2e5
TayGov <Taylor.Clausen@gov.bc.ca>
2020-10-19 14:27:05 -0700

    Adding another endpoint

--- src/backend/quarkus-management-api/src/main/java/com/nttdata/quarkus/management/api/GreetingResource.java
+++ src/backend/quarkus-management-api/src/main/java/com/nttdata/quarkus/management/api/GreetingResource.java
@@ -4,6 +4,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import com.nttdata.pocdata.InventoriesService;
+import com.nttdata.pocdata.hibernate.Contacts;
+import org.springframework.http.ResponseEntity;
 
 @Path("/hello")
 public class GreetingResource {
@@ -11,6 +14,13 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "mello yellow";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/contacts")
+    public ResponseEntity<Contacts> getContacts() {
+        return ResponseEntity.ok(new Contacts());
     }
 }
