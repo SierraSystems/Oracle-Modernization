24a359d5f75332c3055c70cbbdb0d25c3a941285
TayGov <Taylor.Clausen@gov.bc.ca>
2020-10-23 07:58:39 -0700

    Split api

---
+++ src/backend/quarkus-management-api/src/main/java/com/nttdata/quarkus/management/api/controller/ContactsApiServiceImpl.java
@@ -0,0 +1,39 @@
+package com.nttdata.quarkus.management.api.openapi.impl;
+
+import com.nttdata.quarkus.management.api.openapi.*;
+import com.nttdata.quarkus.management.api.openapi.model.*;
+
+
+import java.math.BigDecimal;
+import com.nttdata.quarkus.management.api.openapi.model.Contact;
+
+import java.util.List;
+
+import java.io.InputStream;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaResteasyEapServerCodegen", date = "2020-10-23T07:51:14.462-07:00[America/Los_Angeles]")
+public class ContactsApiServiceImpl implements ContactsApi {
+      public Response addContact(Contact contact,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response deleteContact(BigDecimal contactId,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response getContact(BigDecimal contactId,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response getContacts(SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response updateContact(Contact contact,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+}
---
+++ src/backend/quarkus-management-api/src/main/java/com/nttdata/quarkus/management/api/controller/CustomersApiServiceImpl.java
@@ -0,0 +1,39 @@
+package com.nttdata.quarkus.management.api.openapi.impl;
+
+import com.nttdata.quarkus.management.api.openapi.*;
+import com.nttdata.quarkus.management.api.openapi.model.*;
+
+
+import java.math.BigDecimal;
+import com.nttdata.quarkus.management.api.openapi.model.Customer;
+
+import java.util.List;
+
+import java.io.InputStream;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaResteasyEapServerCodegen", date = "2020-10-23T07:51:14.462-07:00[America/Los_Angeles]")
+public class CustomersApiServiceImpl implements CustomersApi {
+      public Response addCustomer(Customer customer,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response deleteCustomer(BigDecimal customerId,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response getCustomer(BigDecimal customerId,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response getCustomers(SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response updateCustomer(Customer customer,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+}
